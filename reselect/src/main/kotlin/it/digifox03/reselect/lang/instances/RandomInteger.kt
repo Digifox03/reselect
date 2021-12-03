f6dc476904aedb11f07822017d8cade0c00ad293
Digifox03 <digifox03@protonmail.com>
2021-12-03 19:23:13 +0100

    Fix exclusive range

--- reselect/src/main/kotlin/it/digifox03/reselect/lang/instances/RandomInteger.kt
+++ reselect/src/main/kotlin/it/digifox03/reselect/lang/instances/RandomInteger.kt
@@ -4,9 +4,10 @@ import it.digifox03.reselect.lang.core.Instance
 import it.digifox03.reselect.lang.typeclasses.NumericTC
 import it.digifox03.reselect.lang.typeclasses.RandomTC
 import kotlin.random.Random
+import kotlin.random.nextLong
 
 object RandomInteger: RandomTC<Long>, Instance {
 	override fun register() = RandomTC.reg.register("integer", this)
 	override fun rand(random: Random, from: Long, to: Long) =
-		random.nextLong(from, to)
+		random.nextLong(from..to)
 }
